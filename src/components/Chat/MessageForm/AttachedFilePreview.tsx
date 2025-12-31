b79260246f642b6127bc04729a0756fd0484d359
nakib555 <140257355+nakib555@users.noreply.github.com>
2025-12-31 18:07:38 +0600

    fix: Remove unused hover icon from attachment preview
    The eye icon, intended for previewing attached files on hover, is no longer necessary and has been removed from the `AttachedFilePreview` component. This simplifies the UI and removes unused functionality.

--- src/components/Chat/MessageForm/AttachedFilePreview.tsx
+++ src/components/Chat/MessageForm/AttachedFilePreview.tsx
@@ -179,16 +179,6 @@ export const AttachedFilePreview: React.FC<AttachedFilePreviewProps> = ({ file, 
                         </svg>
                     </div>
                 )}
-
-                {/* Hover Eye Icon (Desktop) */}
-                {!hasFailed && !isProcessing && (
-                    <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity duration-200 hidden sm:flex items-center justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-white drop-shadow-md">
-                            <path d="M10 12.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-                            <path fillRule="evenodd" d="M.664 10.59a1.651 1.651 0 0 1 0-1.186A10.004 10.004 0 0 1 10 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0 1 10 17c-4.257 0-7.893-2.66-9.336-6.41ZM14 10a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z" clipRule="evenodd" />
-                        </svg>
-                    </div>
-                )}
             </div>
 
             {/* Floating Remove Button */}
