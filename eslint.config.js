f2ab28f7b3fa8e6f5958c0ba9c142fe2d83cc347
nakib555 <140257355+nakib555@users.noreply.github.com>
2026-01-07 23:06:23 +0600

    feat: Enable react-hooks ESLint plugin
    Integrates the `eslint-plugin-react-hooks` to enforce React hook rules, improving code quality and preventing common bugs.

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,17 @@
 
 import reactCompiler from 'eslint-plugin-react-compiler';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 export default [
   {
     plugins: {
       'react-compiler': reactCompiler,
+      'react-hooks': reactHooks,
     },
     rules: {
       'react-compiler/react-compiler': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
   },
 ];
